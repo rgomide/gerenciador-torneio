26cc2efff338aad9e080c7656b0324e4c04fe4ab
rgomide <renato.s.gomide@gmail.com>
2025-08-28 14:50:47 -0300

    fix

--- frontend/src/components/MatchesComponents/ParticipantsForm.jsx
+++ frontend/src/components/MatchesComponents/ParticipantsForm.jsx
@@ -1,27 +1,25 @@
 'use client'
 
-import { FileUser } from "lucide-react"
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
-import { Button } from "../ui/button"
-import { DialogDescription } from "@radix-ui/react-dialog"
-import { useEffect, useState } from "react"
-import useApi from "@/services/useApi"
+import useApi from '@/services/useApi'
+import { DialogDescription } from '@radix-ui/react-dialog'
+import { FileUser } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import Spinner from '../common/Spinner'
+import { Button } from '../ui/button'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog'
 
-function ParticipantsForm({ matchId, record }) {
+function ParticipantsForm({ record: matchRecord }) {
   const [participants, setParticipants] = useState([])
-  const { getMatchParticipants } = useApi()
-  console.log(matchId);
-
+  const { getMatchParticipants, isLoading } = useApi()
 
   useEffect(() => {
     const fetchParticipants = async () => {
-      try {
-        const data = await getMatchParticipants(matchId)
-        console.log(data);
-        
-        setParticipants(data)
-      } catch (e) {
-        console.error(e);
+      const response = await getMatchParticipants(matchRecord.id)
+
+      if (response.requestSuccessful) {
+        setParticipants(response.data)
+      } else {
+        toast.error(response.error)
       }
     }
 
@@ -40,23 +38,26 @@ function ParticipantsForm({ matchId, record }) {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Participantes da partida</DialogTitle>
-          <DialogDescription>
-            Adicionar participantes à partida.
-          </DialogDescription>
+          <DialogDescription>Adicionar participantes à partida.</DialogDescription>
         </DialogHeader>
 
         <div>
-          <h1 className='font-bold'>Lista de participantes</h1>
-
-          {participants.map((participant) => (
-            <div key={participant.id} className='border p-2 my-2 rounded'>
-              <p className='font-medium'>
-                {participant.participantType === "team"
-                  ? participant.team?.name
-                  : participant.player?.name}
-              </p>
+          <h1 className="font-bold">Lista de participantes</h1>
+          {isLoading ? (
+            <div className="flex justify-center items-center w-full my-4">
+              <Spinner />
             </div>
-          ))}
+          ) : (
+            participants.map((participant) => (
+              <div key={participant.id} className="border p-2 my-2 rounded">
+                <p className="font-medium">
+                  {participant.participantType === 'team'
+                    ? participant.team?.name
+                    : participant.player?.name}
+                </p>
+              </div>
+            ))
+          )}
         </div>
       </DialogContent>
     </Dialog>
