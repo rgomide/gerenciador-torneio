f665164562f40e649652311b177133df2c2eed97
rgomide <renato.s.gomide@gmail.com>
2026-04-15 20:04:19 -0300

    checkpoint 5

--- frontend/src/stories/pages/publicEventPage.stories.js
+++ frontend/src/stories/pages/publicEventPage.stories.js
@@ -6,10 +6,6 @@ import {
   STORY_PUBLIC_EVENT_ID
 } from '../handlers/publicEventPage.handlers'
 
-const nextNavigation = {
-  segments: [['eventId', STORY_PUBLIC_EVENT_ID]]
-}
-
 export default {
   title: 'Pages/PublicEventPage',
   component: PublicEventPage
@@ -17,39 +13,42 @@ export default {
 
 export const Default = {
   name: 'Evento com torneios e partidas',
-  args: {},
   parameters: {
     msw: {
       handlers: publicEventPageHandlersFull
     },
     nextjs: {
-      navigation: nextNavigation
+      navigation: {
+        segments: [['eventId', STORY_PUBLIC_EVENT_ID]]
+      }
     }
   }
 }
 
 export const SemPartidas = {
   name: 'Evento sem partidas',
-  args: {},
   parameters: {
     msw: {
       handlers: publicEventPageHandlersEmptyMatches
     },
     nextjs: {
-      navigation: nextNavigation
+      navigation: {
+        segments: [['eventId', STORY_PUBLIC_EVENT_ID]]
+      }
     }
   }
 }
 
 export const EventoNaoEncontrado = {
   name: 'Evento não encontrado (404)',
-  args: {},
   parameters: {
     msw: {
       handlers: publicEventPageHandlersNotFound
     },
     nextjs: {
-      navigation: nextNavigation
+      navigation: {
+        segments: [['eventId', STORY_PUBLIC_EVENT_ID]]
+      }
     }
   }
 }
